070247ac5e817e24a43ddd080e0ade25a7813150
Freivin Campbell <freivin12@gmail.com>
2021-04-17 19:39:59 -0600

    enable CORS to allow fetching data from out there

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,13 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
 	const app = await NestFactory.create(AppModule);
-	await app.listen(3000);
+	const port = process.env.PORT || 3000;
+
+	app.enableCors();
+
+	await app.listen(port, () => {
+		console.log(`App running on port: ${port} 🚀`);
+	});
 }
+
 bootstrap().catch();
