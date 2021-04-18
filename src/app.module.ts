89ef57d5f63d570a48ead2f2623037849794e2e9
Freivin Campbell <freivin12@gmail.com>
2021-04-18 00:14:17 -0600

    DB connection enabled

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,21 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { envConfiguration } from './lib/config/envConfiguration';
+import * as dotenv from 'dotenv';
+
+dotenv.config();
 
 @Module({
-	imports: [],
+	imports: [
+		ConfigModule.forRoot({
+			isGlobal: true,
+			load: [envConfiguration],
+		}),
+		MongooseModule.forRoot(process.env.DB_URL),
+	],
 	controllers: [AppController],
 	providers: [AppService],
 })
