607e4433b17cd82a76faeeb230e1baef4e5a83f5
Freivin Campbell <freivin12@gmail.com>
2021-04-19 18:18:02 -0600

    pettier format

--- src/statuses/statuses.service.spec.ts
+++ src/statuses/statuses.service.spec.ts
@@ -2,17 +2,17 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { StatusesService } from './statuses.service';
 
 describe('StatusesService', () => {
-  let service: StatusesService;
+	let service: StatusesService;
 
-  beforeEach(async () => {
-    const module: TestingModule = await Test.createTestingModule({
-      providers: [StatusesService],
-    }).compile();
+	beforeEach(async () => {
+		const module: TestingModule = await Test.createTestingModule({
+			providers: [StatusesService],
+		}).compile();
 
-    service = module.get<StatusesService>(StatusesService);
-  });
+		service = module.get<StatusesService>(StatusesService);
+	});
 
-  it('should be defined', () => {
-    expect(service).toBeDefined();
-  });
+	it('should be defined', () => {
+		expect(service).toBeDefined();
+	});
 });
