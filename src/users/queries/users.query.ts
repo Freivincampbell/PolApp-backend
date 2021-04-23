02947024c1fc6fbc9126c70f1c394304186dc9be
Freivin Campbell <freivin12@gmail.com>
2021-04-22 20:31:16 -0600

    querying / Create / find / getById / and update user

--- src/users/queries/users.query.ts
+++ src/users/queries/users.query.ts
@@ -40,15 +40,57 @@ export const findOneQuery = (id: string, currentUser: ICurrentUser) => {
 		case ROLE.CLIENT:
 			query = {
 				_id: id,
-				user: currentUser.id,
-				$or: [{ role: ROLE.AGENT }, { role: ROLE.COSTUMER }],
+				$or: [
+					{ user: currentUser.id },
+					{ user: currentUser.user },
+					{ role: ROLE.AGENT },
+					{ role: ROLE.COSTUMER },
+				],
 			};
 			break;
 		case ROLE.AGENT:
 			query = {
-				_id: {
-					$or: [{ _id: currentUser.id }, { _id: id }],
-				},
+				_id: id,
+				user: currentUser.user,
+				$or: [{ role: ROLE.AGENT }, { role: ROLE.COSTUMER }],
+			};
+			break;
+		case ROLE.SUPERADMIN:
+			query = {
+				_id: id,
+				$or: [
+					{ role: ROLE.SUPERADMIN },
+					{ role: ROLE.CLIENT },
+					{ role: ROLE.AGENT },
+					{ role: ROLE.COSTUMER },
+				],
+			};
+			break;
+		default:
+			query = {};
+	}
+
+	return query;
+};
+
+export const updateQuery = (id: string, currentUser: ICurrentUser) => {
+	let query;
+	switch (currentUser.role) {
+		case ROLE.CLIENT:
+			query = {
+				_id: id,
+				$or: [
+					{ user: currentUser.id },
+					{ user: currentUser.user },
+					{ role: ROLE.CLIENT },
+					{ role: ROLE.AGENT },
+					{ role: ROLE.COSTUMER },
+				],
+			};
+			break;
+		case ROLE.AGENT:
+			query = {
+				_id: id,
 				user: currentUser.user,
 				$or: [{ role: ROLE.AGENT }, { role: ROLE.COSTUMER }],
 			};
